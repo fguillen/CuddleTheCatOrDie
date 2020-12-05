3b48d974002d11a6d2d5728e1000111639e3efda
Fernando Guillen <fguillen.mail@gmail.com>
2020-12-05 15:52:20 +0100

    Scroll limits and hand speed

---
+++ CuddleTheCatOrDie/Assets/HandsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HandsController : MonoBehaviour
+{
+    [SerializeField] Transform[] limits;
+    [SerializeField] float speed;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minX = limits.Min(e => e.position.x);
+        maxX = limits.Max(e => e.position.x);
+        minY = limits.Min(e => e.position.y);
+        maxY = limits.Max(e => e.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsCursor();
+    }
+
+    void MoveTowardsCursor()
+    {
+        Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        // transform.position = new Vector3(worldPosition.x, worldPosition.y, transform.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, worldPosition, speed * Time.deltaTime);
+
+        if(transform.position.x > maxX) transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        if(transform.position.x < minX) transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+        if(transform.position.y > maxY) transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+        if(transform.position.y < minY) transform.position = new Vector3(transform.position.x, minY, transform.position.z);
+    }
+}
