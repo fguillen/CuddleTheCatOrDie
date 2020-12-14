eb17d5d12e1ff4e736089c04ea32f418ff21657b
Fernando Guillen <fguillen.mail@gmail.com>
2020-12-14 14:38:15 +0100

    Menu animation (1)

---
+++ CuddleTheCatOrDie/Assets/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadSceneGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        print("Quiting game");
+        Application.Quit();
+    }
+}
