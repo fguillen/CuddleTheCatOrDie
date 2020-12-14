3f2431a38bcacb7d44a1ff06184ab75ac969a7ca
Fernando Guillen <fguillen.mail@gmail.com>
2020-12-14 17:08:22 +0100

    Menu animation (2)

---
+++ CuddleTheCatOrDie/Assets/CatMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMenuController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    string state;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        state = "grounded";
+    }
+    void Update()
+    {
+        if(state == "grounded")
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        float energy = Random.Range(200, 300);
+        rb.AddForce(transform.up * energy, ForceMode2D.Impulse);
+        state = "jumping";
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("MenuGround"))
+        {
+            state = "grounded";
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
