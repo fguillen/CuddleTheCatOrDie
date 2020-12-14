cc85edfabd82d090dbae664bdd0f8f888eaffa87
Fernando Guillen <fguillen.mail@gmail.com>
2020-12-14 17:29:36 +0100

    Credits page

---
+++ CuddleTheCatOrDie/Assets/MenuCanvasController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCanvasController : MonoBehaviour
+{
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ShowCredits()
+    {
+        animator.SetTrigger("showCredits");
+    }
+
+    public void HideCredits()
+    {
+        animator.SetTrigger("hideCredits");
+    }
+
+    public void ShowSettings()
+    {
+        animator.SetTrigger("showSettings");
+    }
+
+    public void HideSettings()
+    {
+        animator.SetTrigger("hideSettings");
+    }
+
+    public void ShowHigscores()
+    {
+        animator.SetTrigger("showHigscores");
+    }
+
+    public void HideHigscores()
+    {
+        animator.SetTrigger("hideHigscores");
+    }
+}
